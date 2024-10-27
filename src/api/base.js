c78201137e50af410af112f950523c995090d156
koh9904 <koh9904@naver.com>
2024-10-27 09:03:46 +0900

    api 호출방식 변경 및 .env파일 제작

--- src/api/base.js
+++ src/api/base.js
@@ -1,24 +1,28 @@
 import axios from "axios";
 
-// 기본 URL 및 Axios 인스턴스 설정
+// .env 파일에서 API URL 가져오기
+const BASE_URL = process.env.REACT_APP_API_URL; // 환경 변수에서 URL 가져오기
+
+/**
+ * api를 사용할 때, 해당 instance를 사용해주세요.
+ * 자동으로 api를 호출 할 때 token을 header에 넣어서 보내는 코드가 작성되어있습니다.
+ * 궁금하시다면 구글에 axios interceptor 검색해보세요.
+ */
 const axiosInstance = axios.create({
-  baseURL: "http://3.34.182.245:8080", // API 서버 URL 설정
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: BASE_URL, // 환경 변수로 변경
 });
 
-// 요청 시 JWT 토큰 자동 추가
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("jwtToken"); // 토큰 키를 'jwtToken'으로 맞춤
+    const token = localStorage.getItem("token"); // JWT 토큰 가져오기
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
   (error) => {
-    return Promise.reject(error); // 요청 중 오류 발생 시 처리
+    return Promise.reject(error);
   }
 );
 
