ae6662f079627eb7110a17f77a757531c4d813da
koh9904 <koh9904@naver.com>
2024-11-06 16:21:18 +0900

    오류

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Home from "./components/Home";
